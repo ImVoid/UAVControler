c2988df0d2028a75159a15df9dab0ae5f2dcf902
ken <793743423@qq.com>
2016-10-02 09:45:14 +0800

    将开关闪烁由异步消息处理模式改为使用Android封装的AayncTask实现

--- app/src/main/java/top/lightblog/asynctask/SwitchBlinkTask.java
+++ app/src/main/java/top/lightblog/asynctask/SwitchBlinkTask.java
@@ -1,8 +1,77 @@
 package top.lightblog.asynctask;
 
+import android.os.AsyncTask;
+import android.widget.Button;
+
+import top.lightblog.helper.StatusCode;
+import top.lightblog.uavcontroler.R;
+
 /**
  * Created by imliu on 2016/10/2.
  */
 
-public class SwitchBlinkTask {
+public class SwitchBlinkTask extends AsyncTask<Button, Integer, Boolean> {
+
+    private Button btnSwitch;
+
+    @Override
+    protected Boolean doInBackground(Button... buttons) {
+
+        btnSwitch = buttons[0];
+
+        if(StatusCode.switchStatus){    //如果开启了开关则关闭它
+            publishProgress(StatusCode.BLINK_OFF);
+            StatusCode.switchStatus = false;
+        }else{  //关闭了就开启开关，先是快闪然后慢闪
+            StatusCode.switchStatus = true;
+            int lastStatus = StatusCode.BLINK_OFF;  //记录上一次闪烁状态
+            //快闪3下，即6次交替
+            StatusCode.blinkMod = true;    //设置快闪模式
+            for(int i = 0; i < 6; ++i){
+                if(StatusCode.switchStatus) {   //响应开启状态
+                    try {
+                        publishProgress(lastStatus = (lastStatus == StatusCode.BLINK_OFF ? StatusCode.BLINK_ON : StatusCode.BLINK_OFF));
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }else {     //响应关闭状态
+                    publishProgress(StatusCode.BLINK_OFF);
+                    StatusCode.switchStatus = false;
+                    break;
+                }
+            }
+            //循环慢闪
+            StatusCode.blinkMod = false;    //设置慢闪模式
+            while (true){
+                if(StatusCode.switchStatus){    //响应开启状态
+                    try {
+                        Thread.sleep(800);
+                        publishProgress(lastStatus = (lastStatus == StatusCode.BLINK_OFF ? StatusCode.BLINK_ON : StatusCode.BLINK_OFF));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }else {     //响应关闭状态
+                    publishProgress(StatusCode.BLINK_OFF);
+                    StatusCode.switchStatus = false;
+                    break;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        if(!StatusCode.switchStatus ) {  //响应关闭状态时
+            btnSwitch.setBackgroundResource(R.drawable.switch_off);
+        }else{      //响应开启状态时
+            if(StatusCode.blinkMod){    //快闪模式闪蓝灯
+                btnSwitch.setBackgroundResource(values[0] == StatusCode.BLINK_ON ? R.drawable.switch_on : R.drawable.switch_off);
+            }else {     //慢闪模式闪绿灯
+                btnSwitch.setBackgroundResource(values[0] == StatusCode.BLINK_ON ? R.drawable.switch_on_green : R.drawable.switch_off);
+            }
+
+        }
+    }
 }
--- app/src/main/java/top/lightblog/uavcontroler/ControlActivity.java
+++ app/src/main/java/top/lightblog/uavcontroler/ControlActivity.java
@@ -1,5 +1,6 @@
 package top.lightblog.uavcontroler;
 
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import top.lightblog.asynctask.SwitchBlinkTask;
 import top.lightblog.helper.*;
 
 public class ControlActivity extends AppCompatActivity implements View.OnClickListener{
@@ -27,99 +29,12 @@ public class ControlActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.btn_switch:   processBtnSwitch();
+            case R.id.btn_switch:   new SwitchBlinkTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, btnSwitch);
+                System.out.print("abc");
                 break;
             default:
                 break;
         }
     }
 
-    /**
-     * 处理异步消息更新UI
-     */
-    private Handler handler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what){
-                case StatusCode.BLINK_ON:   btnSwitch.setBackgroundResource(StatusCode.blinkMod ? R.drawable.switch_on_green : R.drawable.switch_on);
-                    break;
-                case StatusCode.BLINK_OFF:   btnSwitch.setBackgroundResource(R.drawable.switch_off);
-                    break;
-                default:
-                    break;
-            }
-        }
-    };
-
-    /**
-     * 按钮点击事件处理方法
-     */
-    private void processBtnSwitch(){
-        new Thread(new Runnable() {
-
-            Message message = null;
-            int lastMessage = StatusCode.BLINK_OFF;
-
-            @Override
-            public void run() {
-                //如果打开了则关闭
-                if(StatusCode.switchStatus) {
-                    message = new Message();
-                    message.what = StatusCode.BLINK_OFF;
-                    handler.sendMessage(message);
-                    StatusCode.switchStatus = false;
-                }else { //否则启动
-                    StatusCode.switchStatus = true;
-                    StatusCode.blinkMod = false;    //设置快闪模式
-                    //启动时快闪
-                    for (int i = 0; i < 6; ++i) {
-                        if(StatusCode.switchStatus ) {
-                            startBlink(100);
-                        }else { //中途被关闭
-                            stopBlink();
-                            break;
-                        }
-                    }
-                    //开关慢闪效果
-                    StatusCode.blinkMod = true;     //设置慢闪模式
-                    lastMessage = StatusCode.BLINK_ON;
-                    while (StatusCode.switchStatus){
-                        if(StatusCode.switchStatus ) {
-                            startBlink(800);
-                        }else { //中途被关闭
-                            stopBlink();
-                            break;
-                        }
-                    }
-                }
-            }
-
-            /**
-             * 发送闪烁信息
-             * @param time 闪烁间隔
-             */
-            private void startBlink(int time){
-                message = new Message();
-                message.what = lastMessage == StatusCode.BLINK_OFF ? StatusCode.BLINK_ON : StatusCode.BLINK_OFF;
-                lastMessage = message.what;
-                handler.sendMessage(message);
-                try {
-                    Thread.sleep(time);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            /**
-             * 停止闪烁
-             */
-            private void stopBlink(){
-                message = new Message();
-                message.what = StatusCode.BLINK_OFF;
-                handler.sendMessage(message);
-                StatusCode.switchStatus = false;
-                StatusCode.blinkMod = false;
-            }
-        }).start();
-    }
 }
