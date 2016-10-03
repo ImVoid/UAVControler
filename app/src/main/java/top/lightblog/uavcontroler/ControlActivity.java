9f51ca200d9021866ef00947c33aa0e79260bf97
ken <793743423@qq.com>
2016-10-03 16:57:26 +0800

    添加按钮震动反馈“

--- app/src/main/java/top/lightblog/uavcontroler/ControlActivity.java
+++ app/src/main/java/top/lightblog/uavcontroler/ControlActivity.java
@@ -1,8 +1,10 @@
 package top.lightblog.uavcontroler;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +15,8 @@ import top.lightblog.helper.*;
 
 public class ControlActivity extends AppCompatActivity implements View.OnClickListener{
 
+    //震动传感器
+    private Vibrator vibrator;
     //声明控件
     private Button btnSwitch;
 
@@ -21,6 +25,8 @@ public class ControlActivity extends AppCompatActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         Screen.setFullScreen(this);
 
+        vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+
         btnSwitch = (Button)findViewById(R.id.btn_switch);
         btnSwitch.setOnClickListener(this);
 
@@ -28,6 +34,9 @@ public class ControlActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View view) {
+        //触发按钮震动反馈
+        long [] pattern = {0,25};   // 停止 开启
+        vibrator.vibrate(pattern,-1);
         switch (view.getId()){
             case R.id.btn_switch:   new SwitchBlinkTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, btnSwitch);
                 System.out.print("abc");
