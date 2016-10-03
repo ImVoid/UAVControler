a254c9f73bf696ab1120445b440750ecc34624d6
ken <793743423@qq.com>
2016-10-03 22:47:40 +0800

    nothing

--- app/src/main/java/top/lightblog/uavcontroler/ControlActivity.java
+++ app/src/main/java/top/lightblog/uavcontroler/ControlActivity.java
@@ -1,9 +1,6 @@
 package top.lightblog.uavcontroler;
 
 import android.content.Context;
-import android.os.AsyncTask;
-import android.os.Handler;
-import android.os.Message;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,14 +12,21 @@ import android.widget.SeekBar;
 import java.util.ArrayList;
 import java.util.List;
 
-import top.lightblog.asynctask.SwitchBlinkTask;
+import top.lightblog.businesslogic.BackLogic;
+import top.lightblog.businesslogic.ForwardLogic;
+import top.lightblog.businesslogic.LandingLogic;
+import top.lightblog.businesslogic.LeftLogic;
+import top.lightblog.businesslogic.LuanchLogic;
+import top.lightblog.businesslogic.RightLogic;
+import top.lightblog.businesslogic.SwitchLogic;
 import top.lightblog.helper.*;
 
-public class ControlActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener, SeekBar.OnSeekBarChangeListener{
+public class ControlActivity extends AppCompatActivity
+        implements View.OnClickListener, View.OnTouchListener, SeekBar.OnSeekBarChangeListener {
 
     //震动传感器
     private Vibrator vibrator;
-    long [] pattern = {0,25};   // 震动设置
+    long[] pattern = {0, 25};   // 震动设置
     //声明控件
     private Button btnSwitch;
     private Button btnForward;
@@ -39,23 +43,23 @@ public class ControlActivity extends AppCompatActivity implements View.OnClickLi
         Screen.setFullScreen(this);
 
         //获取震动传感器
-        vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         //获取所有button添加到List中
         List<Button> btnList = new ArrayList<Button>();
-        btnList.add(btnSwitch  = (Button)findViewById(R.id.btn_switch));
-        btnList.add(btnForward = (Button)findViewById(R.id.btn_forward));
-        btnList.add(btnBack = (Button)findViewById(R.id.btn_back));
-        btnList.add(btnLeft = (Button)findViewById(R.id.btn_left));
-        btnList.add(btnRight = (Button)findViewById(R.id.btn_right));
-        btnList.add(btnLaunch = (Button)findViewById(R.id.btn_launch));
-        btnList.add(btnLanding = (Button)findViewById(R.id.btn_landing));
+        btnList.add(btnSwitch = (Button) findViewById(R.id.btn_switch));
+        btnList.add(btnForward = (Button) findViewById(R.id.btn_forward));
+        btnList.add(btnBack = (Button) findViewById(R.id.btn_back));
+        btnList.add(btnLeft = (Button) findViewById(R.id.btn_left));
+        btnList.add(btnRight = (Button) findViewById(R.id.btn_right));
+        btnList.add(btnLaunch = (Button) findViewById(R.id.btn_launch));
+        btnList.add(btnLanding = (Button) findViewById(R.id.btn_landing));
         //所有button添加onClick和onTouch监听器
-        for(Button btn : btnList){
+        for (Button btn : btnList) {
             btn.setOnClickListener(this);
             btn.setOnTouchListener(this);
         }
         //设置SeekBar监听器
-        sbAcc = (SeekBar)findViewById(R.id.sb_acc);
+        sbAcc = (SeekBar) findViewById(R.id.sb_acc);
         sbAcc.setOnTouchListener(this);
         sbAcc.setOnSeekBarChangeListener(this);
     }
@@ -63,36 +67,34 @@ public class ControlActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onClick(View view) {
 
-        switch (view.getId()){
-            case R.id.btn_switch:   new SwitchBlinkTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, btnSwitch);
-                break;
-            case R.id.btn_forward:
-                break;
-            case R.id.btn_back:
-                break;
-            case R.id.btn_left:
-                break;
-            case R.id.btn_right:
+        switch (view.getId()) {
+            case R.id.btn_switch:
+                SwitchLogic.doLogic(btnSwitch);
                 break;
             case R.id.btn_launch:
+                LuanchLogic.doLogic();
                 break;
             case R.id.btn_landing:
+                LandingLogic.doLogic();
                 break;
             default:
                 break;
         }
     }
 
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-
-    }
-
+    //该方法拖动进度条开始拖动的时候调用。
     @Override
     public void onStartTrackingTouch(SeekBar seekBar) {
 
     }
 
+    //该方法拖动进度条进度改变的时候调用
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+    }
+
+    //该方法拖动进度条停止拖动的时候调用
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
 
@@ -100,15 +102,47 @@ public class ControlActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
-        //触发按钮按下震动反馈
-        if(motionEvent.getAction()==MotionEvent.ACTION_DOWN){
-            vibrator.vibrate(pattern,-1);
-        }
-        //触发按钮释放震动反馈
-        if(motionEvent.getAction()==MotionEvent.ACTION_UP){
-            vibrator.vibrate(pattern,-1);
+
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            vibrator.vibrate(pattern, -1);   //触发按钮按下震动反馈
+            switch (view.getId()) {
+                case R.id.btn_forward:
+                    ForwardLogic.doLogic(MotionEvent.ACTION_DOWN);
+                    break;
+                case R.id.btn_back:
+                    BackLogic.doLogic(MotionEvent.ACTION_DOWN);
+                    break;
+                case R.id.btn_left:
+                    LeftLogic.doLogic(MotionEvent.ACTION_DOWN);
+                    break;
+                case R.id.btn_right:
+                    RightLogic.doLogic(MotionEvent.ACTION_DOWN);
+                    break;
+                default:
+                    break;
+            }
         }
 
+        if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+            vibrator.vibrate(pattern, -1);   //触发按钮释放震动反馈
+            switch (view.getId()) {
+                case R.id.btn_forward:
+                    ForwardLogic.doLogic(MotionEvent.ACTION_UP);
+                    break;
+                case R.id.btn_back:
+                    BackLogic.doLogic(MotionEvent.ACTION_UP);
+                    break;
+                case R.id.btn_left:
+                    LeftLogic.doLogic(MotionEvent.ACTION_UP);
+                    break;
+                case R.id.btn_right:
+                    RightLogic.doLogic(MotionEvent.ACTION_UP);
+                    break;
+                default:
+                    break;
+            }
+        }
+        //false:传递给下一个监听器   true:监听事件到此结束
         return false;
     }
 }
